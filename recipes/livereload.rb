ae297290d5075a7b3eac87466b8ffb0f31161353
rafaelorosco <raforosco@gmail.com>
2014-08-20 13:56:05 -0300

    Add livereload recipe

---
+++ recipes/livereload.rb
@@ -0,0 +1,36 @@
+# Application template recipe for the rails_apps_composer. Change the recipe here:
+# https://github.com/polomarte/rails_apps_composer/blob/master/recipes/livereload.rb
+
+add_gem "rack-livereload", group: :development
+add_gem "guard-livereload", group: :development, require: false
+
+stage_three do
+  say_wizard "recipe config Rack-Livereload"
+
+  # Add rack-livereload configuration.
+  insert_into_file 'config/environments/development.rb', after: 'Rails.application.configure do' do
+  "\n
+  MyApp::Application.configure do
+    config.middleware.use Rack::LiveReload
+  end\n"
+  end
+
+  say_wizard 'recipe init Guard-Livereload'
+
+  run 'guard init livereload'
+
+  # Commit initial Livereload's settings
+  git add: '-A' if prefer :git, true
+  git commit: '-qm "Add Livereload settings"' if prefer :git, true
+
+end
+
+__END__
+
+name: livereload
+description: "Add Livereload packages"
+author: polomarte
+
+requires: [tests]
+run_after: [tests]
+category: development
