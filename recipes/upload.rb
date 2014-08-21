096222f01415a9e92354a6c7784bfdd527e1b07c
rafaelorosco <raforosco@gmail.com>
2014-08-21 17:33:24 -0300

    Get host name by prefs in Upload

--- recipes/upload.rb
+++ recipes/upload.rb
@@ -4,6 +4,7 @@
 add_gem 'fog'
 add_gem 'carrierwave'
 add_gem 'mini_magick'
+add_gem 'aws-sdk'
 
 stage_three do
   say_wizard "recipe installing Carrierwave"
@@ -15,6 +16,19 @@ stage_three do
 # Copy default Carrierwave uploader
   repo = 'https://raw.github.com/polomarte/polomarte_composer/master'
   copy_from "#{repo}/carrierwave/uploader.rb", 'app/uploaders/image_uploader.rb'
+
+# Amazon Web Services Configuration
+  require 'aws-sdk'
+
+  AWS.config(
+    access_key_id: ENV['POLOMARTE_COMPOSER_AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['POLOMARTE_COMPOSER_AWS_SECRET_ACCESS_KEY_ID']
+  )
+
+  s3 = AWS::S3.new
+  s3.buckets.create("assets.production.#{prefs[:host_domain]}")
+  s3.buckets.create("assets.staging.#{prefs[:host_domain]}")
+
 
   git add: '-A' if prefer :git, true
   git commit: '-qm "Add Upload settings"' if prefer :git, true
