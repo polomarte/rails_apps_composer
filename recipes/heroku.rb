5887d07b0144bb27521856ef336e588b7612dd49
rafaelorosco <raforosco@gmail.com>
2014-08-28 19:49:32 -0300

    Updated placeholder in application.yml

--- recipes/heroku.rb
+++ recipes/heroku.rb
@@ -8,32 +8,32 @@ add_gem 'heroku', group: :development
 stage_three do
   say_wizard "recipe config Heroku keys"
 
-  gsub_file 'config/application.yml', "__host_domain", "#{prefs[:host_domain]}"
-  gsub_file 'config/application.yml', "__aws_key_id", "#{prefs[:aws_key_id]}"
-  gsub_file 'config/application.yml', "__aws_secret_key", "#{prefs[:aws_secret_key]}"
+  gsub_file 'config/application.yml', "{{host_name}}", "#{prefs[:host_domain]}"
+  gsub_file 'config/application.yml', "{{aws_key_id}}", "#{prefs[:aws_key_id]}"
+  gsub_file 'config/application.yml', "{{aws_secret_key}}", "#{prefs[:aws_secret_key]}"
   gsub_file 'config/application.yml', "admin@domain_name", "admin@#{prefs[:host_domain]}"
   gsub_file 'config/application.yml', "**app_name**", "**#{@app_name}**"
-  gsub_file 'config/application.yml', "__new_relic_key", "#{prefs[:new_relic_key]}"
-  gsub_file 'config/application.yml', "__rollbar_token", "#{prefs[:rollbar_token]}"
-  gsub_file 'config/application.yml', "__rollbar_token_client", "#{prefs[:rollbar_token_client]}"
+  gsub_file 'config/application.yml', "{{new_relic_key}}", "#{prefs[:new_relic_key]}"
+  gsub_file 'config/application.yml', "{{rollbar_token}}", "#{prefs[:rollbar_token]}"
+  gsub_file 'config/application.yml', "{{rollbar_token_client}}", "#{prefs[:rollbar_token_client]}"
 
-  gsub_file 'config/application.yml', "__dev_mailtrap_user", "#{prefs[:dev_mailtrap_user]}"
-  gsub_file 'config/application.yml', "__dev_mailtrap_password", "#{prefs[:dev_mailtrap_password]}"
-  gsub_file 'config/application.yml', "__dev_secret_key", "#{prefs[:dev_secret_key]}"
+  gsub_file 'config/application.yml', "{{dev_mailtrap_user}}", "#{prefs[:dev_mailtrap_user]}"
+  gsub_file 'config/application.yml', "{{dev_mailtrap_password}}", "#{prefs[:dev_mailtrap_password]}"
+  gsub_file 'config/application.yml', "{{dev_secret_key}}", "#{prefs[:dev_secret_key]}"
 
-  gsub_file 'config/application.yml', "__test_secret_key", "#{prefs[:test_secret_key]}"
+  gsub_file 'config/application.yml', "{{test_secret_key}}", "#{prefs[:test_secret_key]}"
 
-  gsub_file 'config/application.yml', "__staging_host", "#{prefs[:staging_host]}"
-  gsub_file 'config/application.yml', "__staging_fog_dir", "#{prefs[:staging_fog_dir]}"
-  gsub_file 'config/application.yml', "__staging_mailtrap_user", "#{prefs[:staging_mailtrap_user]}"
-  gsub_file 'config/application.yml', "__staging_mailtrap_pwd", "#{prefs[:staging_mailtrap_pwd]}"
-  gsub_file 'config/application.yml', "__staging_secret_key", "#{prefs[:staging_secret_key]}"
+  gsub_file 'config/application.yml', "{{staging_host}}", "#{prefs[:staging_host]}"
+  gsub_file 'config/application.yml', "{{staging_fog_dir}}", "#{prefs[:staging_fog_dir]}"
+  gsub_file 'config/application.yml', "{{staging_mailtrap_user}}", "#{prefs[:staging_mailtrap_user]}"
+  gsub_file 'config/application.yml', "{{staging_mailtrap_pwd}}", "#{prefs[:staging_mailtrap_pwd]}"
+  gsub_file 'config/application.yml', "{{staging_secret_key}}", "#{prefs[:staging_secret_key]}"
 
-  gsub_file 'config/application.yml', "__prod_host", "#{prefs[:prod_host]}"
-  gsub_file 'config/application.yml', "__prod_fog_dir", "#{prefs[:prod_fog_dir]}"
-  gsub_file 'config/application.yml', "__prod_mailtrap_user", "#{prefs[:prod_mailtrap_user]}"
-  gsub_file 'config/application.yml', "__prod_mailtrap_pwd", "#{prefs[:prod_mailtrap_pwd]}"
-  gsub_file 'config/application.yml', "__prod_secret_key", "#{prefs[:prod_secret_key]}"
+  gsub_file 'config/application.yml', "{{prod_host}}", "#{prefs[:prod_host]}"
+  gsub_file 'config/application.yml', "{{prod_fog_dir}}", "#{prefs[:prod_fog_dir]}"
+  gsub_file 'config/application.yml', "{{prod_mailtrap_user}}", "#{prefs[:prod_mailtrap_user]}"
+  gsub_file 'config/application.yml', "{{prod_mailtrap_pwd}}", "#{prefs[:prod_mailtrap_pwd]}"
+  gsub_file 'config/application.yml', "{{prod_secret_key}}", "#{prefs[:prod_secret_key]}"
 
   git add: '-A' if prefer :git, true
   git commit: '-qm "rails_apps_composer: Add Heroku settings"' if prefer :git, true
