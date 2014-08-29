0440163224db471744e016d6f1284820b7467cae
rafaelorosco <raforosco@gmail.com>
2014-08-29 16:44:44 -0300

    Add default locale files

--- recipes/i18n.rb
+++ recipes/i18n.rb
@@ -6,9 +6,35 @@ add_gem 'route_translator'
 stage_three do
   say_wizard "recipe installing Route Translator"
 
-  # Copy default Carrierwave settings
+  # Copy default Route Translator settings
   repo = 'https://raw.github.com/polomarte/polomarte_composer/master'
   copy_from "#{repo}/i18n/route_translator.rb", 'config/initializers/route_translator.rb'
+
+  # Copy default locale files
+  copy_from "#{repo}/locales/models.pt-BR.yml", 'config/locales/models.pt-BR.yml'
+  copy_from "#{repo}/locales/models.en.yml", 'config/locales/models.en.yml'
+
+  copy_from "#{repo}/locales/admin.forms.pt-BR.yml", 'config/locales/admin.forms.pt-BR.yml'
+
+  repo = 'https://raw.github.com/justinfrench/formtastic/master/'
+  copy_from "#{repo}/lib/locale/en.yml", 'config/locales/admin.forms.en.yml'
+
+  repo = 'https://raw.github.com/activeadmin/activeadmin/master'
+  copy_from "#{repo}/config/locales/pt-BR.yml", 'config/locales/active_admin.pt-BR.yml'
+  copy_from "#{repo}/config/locales/en.yml", 'config/locales/active_admin.en.yml'
+
+  repo = 'https://raw.github.com/svenfuchs/rails-i18n/master'
+  copy_from "#{repo}/rails/locale/pt-BR.yml", 'config/locales/rails.pt-BR.yml'
+  copy_from "#{repo}/rails/locale/en.yml", 'config/locales/rails.en.yml'
+
+  repo = 'https://raw.github.com/tigrish/devise-i18n/master'
+  copy_from "#{repo}/locales/pt-BR.yml", 'config/locales/devise.pt-BR.yml'
+  copy_from "#{repo}/locales/en.yml", 'config/locales/devise.en.yml'
+
+  remove_file 'config/locales/pt-BR.yml'
+  remove_file 'config/locales/en.yml'
+
+
 
   git add: '-A' if prefer :git, true
   git commit: '-qm "Add i18n settings"' if prefer :git, true
