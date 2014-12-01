c4cd62ae8bce171b6134637f2a3292793bbf1e0f
Theo Bittencourt <theodrums@gmail.com>
2014-12-01 16:27:35 -0200

    Cleanup Heroku recipe

--- recipes/heroku.rb
+++ recipes/heroku.rb
@@ -1,42 +1,8 @@
 # Application template recipe for the rails_apps_composer. Change the recipe here:
 # https://github.com/polomarte/rails_apps_composer/blob/master/recipes/heroku.rb
 
-add_gem 'rails_12factor', group: [:production, :staging]
-add_gem 'heroku-deflater', group: [:production, :staging]
-add_gem 'heroku', group: :development
-
 stage_three do
-  say_wizard "recipe config Heroku keys"
-
-  gsub_file 'config/application.yml', "{{host_name}}", "#{prefs[:host_domain]}"
-  gsub_file 'config/application.yml', "{{aws_key_id}}", "#{prefs[:aws_key_id]}"
-  gsub_file 'config/application.yml', "{{aws_secret_key}}", "#{prefs[:aws_secret_key]}"
-  gsub_file 'config/application.yml', "admin@domain_name", "admin@#{prefs[:host_domain]}"
-  gsub_file 'config/application.yml', "**app_name**", "**#{@app_name}**"
-  gsub_file 'config/application.yml', "{{new_relic_key}}", "#{prefs[:new_relic_key]}"
-  gsub_file 'config/application.yml', "{{rollbar_token}}", "#{prefs[:rollbar_token]}"
-  gsub_file 'config/application.yml', "{{rollbar_token_client}}", "#{prefs[:rollbar_token_client]}"
-
-  gsub_file 'config/application.yml', "{{dev_mailtrap_user}}", "#{prefs[:dev_mailtrap_user]}"
-  gsub_file 'config/application.yml', "{{dev_mailtrap_password}}", "#{prefs[:dev_mailtrap_password]}"
-  gsub_file 'config/application.yml', "{{dev_secret_key}}", "#{prefs[:dev_secret_key]}"
-
-  gsub_file 'config/application.yml', "{{test_secret_key}}", "#{prefs[:test_secret_key]}"
-
-  gsub_file 'config/application.yml', "{{staging_host}}", "#{prefs[:staging_host]}"
-  gsub_file 'config/application.yml', "{{staging_fog_dir}}", "#{prefs[:staging_fog_dir]}"
-  gsub_file 'config/application.yml', "{{staging_mailtrap_user}}", "#{prefs[:staging_mailtrap_user]}"
-  gsub_file 'config/application.yml', "{{staging_mailtrap_pwd}}", "#{prefs[:staging_mailtrap_pwd]}"
-  gsub_file 'config/application.yml', "{{staging_secret_key}}", "#{prefs[:staging_secret_key]}"
-
-  gsub_file 'config/application.yml', "{{prod_host}}", "#{prefs[:prod_host]}"
-  gsub_file 'config/application.yml', "{{prod_fog_dir}}", "#{prefs[:prod_fog_dir]}"
-  gsub_file 'config/application.yml', "{{prod_mailtrap_user}}", "#{prefs[:prod_mailtrap_user]}"
-  gsub_file 'config/application.yml', "{{prod_mailtrap_pwd}}", "#{prefs[:prod_mailtrap_pwd]}"
-  gsub_file 'config/application.yml', "{{prod_secret_key}}", "#{prefs[:prod_secret_key]}"
-
-  git add: '-A' if prefer :git, true
-  git commit: '-qm "rails_apps_composer: Add Heroku settings"' if prefer :git, true
+  say_wizard 'recipe config Heroku'
 end
 
 __END__
