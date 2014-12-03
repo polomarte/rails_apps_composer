b0297fe91549b642d83e7d36cb00737716e3e4a7
Theo Bittencourt <theodrums@gmail.com>
2014-12-03 16:55:11 -0200

    Create rollbar recipe

---
+++ recipes/rollbar.rb
@@ -0,0 +1,33 @@
+# Application template recipe for the rails_apps_composer. Change the recipe here:
+# https://github.com/polomarte/rails_apps_composer/blob/master/recipes/environment.rb
+
+def repo
+  'https://raw.github.com/polomarte/polomarte_composer/master'
+end
+
+stage_three do
+  run "rails generate rollbar #{prefs[:rollbar_token]}"
+
+  if prefer :templates, 'slim'
+    copy_from "#{repo}/views/application/_rollbar.slim", 'app/views/application/_rollbar.slim'
+
+    insert_into_file 'app/views/layouts/application.html.slim', before: "== javascript_include_tag 'application'" do
+      "== render 'layouts/rollbar'\n    "
+    end
+  else
+    say 'Template setting is not Slim. Add layouts/_rollbar manually'
+  end
+
+  git add: '-A' if prefer :git, true
+  git commit: '-qm "rails_apps_composer: Rollbar setup"' if prefer :git, true
+end
+
+__END__
+
+name: environment
+description: "Change Environment files"
+author: polomarte
+
+requires: [heroku, frontend]
+run_after: [heroku, frontend]
+category: configuration
