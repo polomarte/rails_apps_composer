66dc15056d170c741edd11e35903b8bde2571d18
Theo Bittencourt <theodrums@gmail.com>
2014-12-04 15:09:21 -0200

    Update gems_marte recipe - jazz_hands must be ruby 2 compatible by using pry-byebug

--- recipes/gems_marte.rb
+++ recipes/gems_marte.rb
@@ -1,6 +1,10 @@
 add_gem 'draper', '~> 1.3'
-add_gem 'jazz_hands', :group => [:development, :test]
-add_gem 'pry-rescue', :group => [:development, :test]
+add_gem 'jazz_hands',
+  group:  [:development, :test],
+  github: 'nixme/jazz_hands',
+  branch: 'bring-your-own-debugger'
+add_gem'pry-byebug'
+add_gem 'pry-rescue', group: [:development, :test]
 add_gem 'rails_12factor', group: [:production, :staging]
 add_gem 'heroku-deflater', group: [:production, :staging]
 add_gem 'heroku', group: :development
