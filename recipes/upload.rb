45f959985495df3060dfd01f7b072bebb2fdd204
Theo B <mail@theob.me>
2016-06-10 14:05:24 -0300

    Update upload recipe - Add public/uploads to .gitignore

--- recipes/upload.rb
+++ recipes/upload.rb
@@ -104,6 +104,8 @@ stage_two do
           \"s3:AbortMultipartUpload\",\"s3:DeleteObject\",\"s3:DeleteObjectVersion\",\"s3:GetBucketAcl\",\"s3:GetBucketCORS\",\"s3:GetObject\",\"s3:GetObjectAcl\",\"s3:GetObjectVersion\",\"s3:GetObjectVersionAcl\",\"s3:ListBucket\",\"s3:ListBucketMultipartUploads\",\"s3:ListBucketVersions\",\"s3:ListMultipartUploadParts\",\"s3:PutObject\",\"s3:PutObjectAcl\",\"s3:PutObjectVersionAcl\",\"s3:RestoreObject\"]}]}"
   })
 
+  append_to_file '.gitignore', "# Ignore public/uploads\n/public/uploads"
+
   git add: '-A' if prefer :git, true
   git commit: '-qm "rails_apps_composer: Add Upload settings"' if prefer :git, true
 end
