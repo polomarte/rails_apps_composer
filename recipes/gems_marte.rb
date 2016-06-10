a8406bb4f04cb496589dbe2f3966162b450b6dac
Theo B <mail@theob.me>
2016-06-10 14:19:33 -0300

    Add pg_search to gems_marte recipe

--- recipes/gems_marte.rb
+++ recipes/gems_marte.rb
@@ -1,15 +1,13 @@
 add_gem 'cocoon', '~> 1.2.6'
 add_gem 'bourbon', '~>3.2.3' # Active Admin compat
 add_gem 'draper', '~> 1.3'
-add_gem 'jazz_hands',
-  group:  [:development, :test],
-  github: 'nixme/jazz_hands',
-  branch: 'bring-your-own-debugger'
-add_gem'pry-byebug'
+add_gem 'jazz_hands', group:  [:development, :test], github: 'nixme/jazz_hands', branch: 'bring-your-own-debugger'
+add_gem 'pry-byebug'
 add_gem 'pry-rescue', group: [:development, :test]
 add_gem 'rails_12factor', group: [:production, :staging]
 add_gem 'heroku-deflater', group: [:production, :staging]
 add_gem 'activeadmin-wysihtml5', github: 'theo-bittencourt/activeadmin-wysihtml5'
+add_gem 'pg_search', '~> 1.0.5'
 
 __END__
 
